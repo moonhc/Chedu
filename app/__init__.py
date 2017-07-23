a270200ed357981e4f31eab8e8bd356c825bf9e8
moonhc <mhc3357@gmail.com>
2017-07-23 10:21:56 +0000

    Database wrapper functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
+from __future__ import with_statement
+from contextlib import closing
+import sqlite3
 from flask import Flask
 from flask_socketio import SocketIO
 
+# Configuration for database
+DATABASE = '/home/chedu/Chedu/app/database/chedu.db'
+DEBUG = False
+SECRET_KEY = "Yellow Slug"
+USERNAME = "Chedu"
+PASSWORD = "slug123"
+
+# Create application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'slug'
+app.config.from_object(__name__)
 socketio = SocketIO(app)
 
 from app import routes
