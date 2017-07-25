6d1591315e6451c64c9a114682296161e8118521
DonghyunAhn <segaukwa@kaist.ac.kr>
2017-07-25 14:07:14 +0000

    Live chat function implemented

--- app/static/chat.js
+++ app/static/chat.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
   uname = $('#data').data('uname')
   socket = io.connect('http://52.53.49.26')
 
+  function send_message() {
+    $('.msg-input').focus()
+    var msg = $('.msg-input').val()
+    if (msg != '') {
+      socket.send({msg: msg, room: passcode, uname: uname, type: 'msg'})
+      $('.msg-input').val('')
+    }
+  }
+
   socket.on('connect', function() {
     socket.emit('join', {room: passcode, uname: uname})
   })
@@ -27,13 +36,15 @@ $(document).ready(function() {
            </li>')
         break
     }
+    $(".messages-wrapper").stop().animate(
+      { scrollTop: $(".messages-wrapper")[0].scrollHeight}, 1000)
   })
 
-  $('.emoji-icon').on('click', function() {
-    var msg = $('.msg-input').val()
-    if (msg != '') {
-      socket.send({msg: msg, room: passcode, uname: uname, type: 'msg'})
-      $('.msg-input').val('')
+  $('.emoji-icon').on('click', send_message)
+  $('.msg-input').on('keypress', function(e) {
+    if (e.keyCode==13) {
+      send_message()
+      return false
     }
   })
 })
