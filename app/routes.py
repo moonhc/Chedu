7ccc02ea526a8eb43cb7c08dc1b74953f1ae476e
moonhc <mhc3357@gmail.com>
2017-07-25 06:43:33 +0000

    add to database when file is uploaded

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-import sqlite3
-import os
+import sqlite3, os, random, string, pytz
+from datetime import datetime
 from flask import render_template, request, url_for, redirect, g, jsonify, send_from_directory
 from flask_socketio import send
 from werkzeug import secure_filename
@@ -35,12 +35,25 @@ def get_db():
         db = g._database = connect_db()
     return db
 
-# Query wrapper function
+# Simple query wrapper function
 def query_db(query, args=(), one=False):
     cursor = get_db().execute(query, args)
     rv = cursor.fetchall()
     cursor.close()
     return (rv[0] if rv else None) if one else rv
+
+# Insert wrapper function
+def insert_db(table_name, args):
+    query = 'insert into ' + table_name
+    if table_name == 'files' and len(args) == 3:
+        query += ' values (?, ?, ?)'
+    elif table_name == 'chat_log' and len(args) == 4:
+        query += ' (passcode, user_email, message, chat_date) values (?, ?, ?, ?)'
+    else:
+        return error_handling()
+    db = get_db()
+    db.execute(query, args)
+    db.commit()
 
 # Close the connection to database
 def close_db():
@@ -49,12 +62,24 @@ def close_db():
         if db is not None:
             db.close()
 
-# Request error handling
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+#############################
+#      Useful functions     #
+#############################
+
+def make_passcode():
+    while True:
+        passcode = ''.join(random.choice(string.letters) for _ in xrange(8))
+        if validate_passcode(passcode):
+            return passcode
+
+def validate_passcode(passcode):
+    if passcode is None:
+        return False
+
+    files_entry = query_db('select * from files where passcode = ?', [passcode], one=True)
+    if files_entry is None:
+        return True
+    return False
 
 #############################
 #           Routing         #
@@ -87,12 +112,16 @@ def upload_file():
             file = request.files['file']
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                passcode = make_passcode()
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], passcode+filename))
+                timezone = pytz.timezone('US/Pacific')
+                current_time = datetime.now(timezone).strftime('%y-%m-%d')
+                insert_db('files', [passcode, filename, current_time])
                 return redirect(url_for('chat',
-                                        passcode='tmp', uname='Owner'))
+                                        passcode=passcode, uname='Owner'))
         return redirect(url_for('index'))
     except:
-        return 'Error'
+        return error_handling()
 
 # Give a access for uploaded files
 @app.route('/uploads/<filename>')
@@ -103,20 +132,29 @@ def uploaded_file(filename):
 # Render chat page with correct password.
 @app.route('/chat/<passcode>')
 def chat(passcode, uname=None):
-    if passcode == None :
+    if validate_passcode(passcode):
         return error_handling()
+
+    file_info = query_db('select * from files where passcode = ?', [passcode], one=True)
     return render_template('chedu.html', passcode=passcode, uname=uname, message=None,
-                                filename="TEST", filedate="2017-00-00")
+                                filename=file_info['file_name'], filedate=file_info['open_date'])
 
 # Render decription page.
 @app.route('/about')
 def show_about():
     return render_template('about.html')
 
+# Request error handling
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 # Socket for message. After recieve message from 'msg'
 # and publish to all the users who see the same ppt.
 # guest_cnt = 0
-@socketio.on('message')
+@socketio.on('message', namespace='/test')
 def message_handler(msg):
     # if msg == 'new user':
     #     global guest_cnt
